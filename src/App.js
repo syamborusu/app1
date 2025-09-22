69e5bfbbd79719bba531327d3d0fab1cbfefeeaa
syam <borusulokesh0350@gmail.com>
2025-09-22 20:17:30 +0530

    AUG26/2025

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import Events from './Events';
-import Counter from './Counter';
-import ReadText from './ReadText';
-import Todolist from './Todolist';
+import StyleComp1 from './StyleComp1';
+import StyleComp2 from './StyleComp2';
+import StyleComp3 from './StyleComp3';
+import StyleComp4 from './StyleComp4';
+import StyleComp5 from './StyleComp5';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>React App development</h1>
       </header>
-      <Events/>
-      <Counter/>
-      <ReadText/>
-      <Todolist/>
+      <StyleComp1/>
+      <StyleComp2/>
+      <StyleComp3/>
+      <StyleComp4/>
+      <StyleComp5/>
     </div>
   );
 }
